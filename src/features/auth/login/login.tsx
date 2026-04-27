8b28fda23a66a4ffa56066af2623205e9e37792f
Mohamad Jamal <oussgool33@gmail.com>
2026-04-28 01:17:10 +0300

    new folder added

---
+++ src/features/auth/login/login.tsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="login-container">
+      <form action="/login" method="POST">
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
